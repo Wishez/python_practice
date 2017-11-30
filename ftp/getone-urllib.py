e3cec171c9de10a7f8abfcb7e2cb7b18d9a9684c
Filipp Zhuravlev <shiningfinger@list.ru>
2017-11-30 08:42:36 +0300

    Exercised ftp with urllib.

---
+++ ftp/getone-urllib.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+import os, getpass, sys
+from urllib.request import urlopen # веб инструмент на основе сокетов
+filename = 'cat.jpg'
+print(sys.argv)
+if len(sys.argv) > 1:
+	filename = sys.argv[1]
+
+password = getpass.getpass('Pswd?')
+
+remoteaddr = 'ftp://root:%s@194.67.216.135/%s;type=i' % (password, filename)
+print('Downloading', remoteaddr)
+
+# urllib.request.urlretreive(remoteaddr, filename)
+
+remotefile = urlopen(remoteaddr)
+localfile = open(filename, 'wb')
+localfile.write(remotefile.read())
+localfile.close()
+remotefile.close()
